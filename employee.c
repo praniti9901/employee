927c656c3324ae47dffec8cdf727a3e0243247b2
praniti9901 <78950459+praniti9901@users.noreply.github.com>
2021-02-24 21:45:47 +0530

    details of employee
    name,age,salary,phone nnumber details of employee

---
+++ employee.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct employee
+{
+char Name[20];
+int age;
+int phonenumber;
+int salary;
+}employee;
+int main()
+{
+int i,n=20,a,b,c;
+ printf("\tName\tAge\tPhone Number\tSalary\n",n);
+ 
+        printf("\tChirag\t24\t1234567788\t20000\n");
+        printf("\tArnav\t31\t1234567891\t56000\n");
+        printf("\tShivam\t45\t8881101111\t30500");
+}
+ 
+    
+ 
+
+   
